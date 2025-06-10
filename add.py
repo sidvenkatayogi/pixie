1afd57ab937b5fab68e0f944ac8f23b6bda6947a
Siddhartha Venkatayogi <sidvenkatayogi@gmail.com>
2025-06-09 23:29:24 -0500

    adding to db done

---
+++ add.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import chromadb
+from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
+from chromadb.utils.data_loaders import ImageLoader
+import numpy as np
+from tqdm import tqdm
+import os
+
+# db_path = ""
+
+# client = chromadb.PersistentClient(path=db_path)
+client = chromadb.PersistentClient()
+
+collection = client.get_or_create_collection(
+    name= "multimodal_collection",
+    embedding_function= OpenCLIPEmbeddingFunction(),
+    data_loader= ImageLoader()
+    )
+
+def add_images_to_collection(folder_path, explore = False):
+
+    image_paths = []
+
+    # [os.path.join(folder_path, file) for file in os.listdir(folder_path) 
+    #                if os.path.isfile(os.path.join(folder_path, file)) and file.lower().endswith((""))]
+    
+    for file in tqdm(os.listdir(folder_path), desc= f"Exploring... ({folder_path})", disable= not explore):
+        # print(folder_path)
+        file_path = os.path.join(folder_path, file)
+        if explore and os.path.isdir(file_path):
+            add_images_to_collection(file_path, explore)
+        elif os.path.isfile(file_path) and file.lower().endswith((".png", ".jpg", ".jpeg")):
+            image_paths.append(file_path)
+
+    if (len(image_paths) > 0):
+        for image_path in tqdm(image_paths, desc= f"Creating Embeddings and Adding to DB... ({folder_path})"):
+            try:
+                image = np.array(Image.open(image_path))
+                collection.add(
+                    ids= [os.path.basename(image_path)],
+                    images= [image]
+                )
+            except Exception as e:
+                print(f"Error processing {image_path}: {e}")
+
+
+image_folder_path= "images"
+
+add_images_to_collection(image_folder_path, explore= True)
