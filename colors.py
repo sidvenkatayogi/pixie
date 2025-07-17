e5616bc39dc60b250eadf6670ecf6912354d2415
Siddhartha Venkatayogi <sidvenkatayogi@gmail.com>
2025-07-17 18:54:57 -0500

    add caching to multidist

--- colors.py
+++ colors.py
@@ -33,7 +33,7 @@ def get_dominant_colors(image, palette_size=16, num_colors=5):
 
     i = 0
     while len(dominant_colors) < num_colors and i < len(color_counts):
-        if color_counts[i][0] > (0.05 * max): # if color is too insignificant, not dominant
+        if color_counts[i][0] > (0.025 * max): # if color is too insignificant, not dominant
             palette_index = color_counts[i][1]
 
             # palette is 1 x (3*num palette colors)
@@ -98,8 +98,7 @@ def wdist(c1, c2):
 
 def labdist(c1, c2):
     """
-    Get euclidean color distance in LAB color space between 2 RGB vectors\n
-    formula: https://www.compuphase.com/cmetric.htm#:~:text=A%20low%2Dcost%20approximation)
+    Get euclidean color distance in LAB color space between 2 RGB vectors
 
     Args:
         c1 (list or numpy.ndarray): first/starting RGB vector
@@ -108,11 +107,9 @@ def labdist(c1, c2):
     Returns:
         float
     """
-    # Ensure RGB is normalized (0–1) for skimage
     rgb1 = np.array(c1[:3]) / 255.0
     rgb2 = np.array(c2[:3]) / 255.0
 
-    # Convert to Lab
     lab1 = rgb2lab([[rgb1]])[0][0]
     lab2 = rgb2lab([[rgb2]])[0][0]
 
@@ -126,9 +123,9 @@ def multidist(i1, i2):
     Get color distance of 2 color vectors representing multiple colors by weighting distances by frequency of color
 
     Args:
-        c1 (list or numpy.ndarray): first/starting RGBF vector with the following structure:
+        i1 (list or numpy.ndarray): first/starting RGBF vector with the following structure:
             [red (of most dominant color), blue, green, frequency, red (of 2nd most dominant color), blue, green, frequency, ...]
-        c2 (list or numpy.ndarray): second/end RGBF vector with the following structure:
+        i2 (list or numpy.ndarray): second/end RGBF vector with the following structure:
             [red (of most dominant color), blue, green, frequency, red (of 2nd most dominant color), blue, green, frequency, ...]
 
     Returns:
@@ -138,13 +135,23 @@ def multidist(i1, i2):
     i2 = i2.reshape(int(len(i2)/4), 4)
     distance = 0
     tf = 0
+    d = {}
     for j, rgbf1 in enumerate(i1): # i also tried a rank wise weighting using j and k but it didn't work as well
         for k, rgbf2 in enumerate(i2):
-            f1 = rgbf1[-1]
-            f2 = rgbf2[-1]
-            distance += labdist(rgbf1[:3], rgbf2[:3]) * np.sqrt(f1 * f2) # weight by geometric mean of the 2 frequencies
-            tf += np.sqrt(f1 * f2)
-    distance /= tf # normalize
+            # cache each pair bc labdist is symmetric
+            key = tuple([tuple(rgbf1), tuple(rgbf2)].sort())
+            if key in d:
+                distance += d[key][0]
+                tf += d[key][1]
+            else:
+                f1 = rgbf1[-1]
+                f2 = rgbf2[-1]
+                gmf = np.sqrt(f1 * f2)
+                labd = labdist(rgbf1[:3], rgbf2[:3]) * gmf # weight by geometric mean of the 2 frequencies
+                distance += labd
+                tf += gmf
+                d[key] = (labd, gmf)
+    distance = (distance / tf) if tf != 0 else 0 # normalize
     return distance
 
 
